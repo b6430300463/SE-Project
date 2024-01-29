e04628863feae0391eb3f0abd32dc17e05d344c8
b6430300901 <wiphawee.la@ku.th>
2024-01-29 20:38:21 +0700

    update Login

---
+++ src/Screen/Login.js
@@ -0,0 +1,91 @@
+import './Style/loginStyle.css'
+import {GoogleLogin ,GoogleLogout} from 'react-google-login'
+import { gapi } from 'gapi-script'
+import { useState , useEffect } from 'react'
+
+const Login = () =>{
+    const clientId = "627023661930-9mt9l2l94ki17cenpkv27947ggm24iif.apps.googleusercontent.com"
+    
+    const [profile, setProfile] = useState(null)
+    useEffect(() => {
+        const initClient =() => {
+            gapi.client.init({
+                clientId : clientId,
+                scope : ''
+            })
+        }
+        gapi.load("client:auth2", initClient)
+    },[])
+
+    const onSuccess = (res) => {
+        setProfile(res.profileObj)
+        console.log('success',res)
+    }
+    const onFailure = (res) => {
+        console.log('failed',res)
+    }
+    const logOut =() =>{
+        setProfile(null);
+    }
+    return(
+        <div className = "login-container" >
+            <div className ="screen1">
+                <div className ="screen2">
+                    <div className = "screen3">
+                        <img src="logo_ku.png"  className="logo-image" /> 
+                        <br /><br />
+                        {profile ? (
+                            <div >
+                                <div className ="user-image">
+                                <img src ={profile.imageUrl} alt="user image"/>
+                                </div>
+                                <div className = "email-container">
+
+                                    <p>Name : {profile.name}</p>
+                                    <p>Email : {profile.email}</p>
+                                </div>     
+                                <br/><br/>
+                                <div className='google-logout-button'>
+                                    <GoogleLogout 
+                                        clientId = {clientId}
+                                        buttonText="Log out" 
+                                        onLogoutSuccess={logOut}
+                                        
+                                    />
+                                </div>
+                                
+                            </div>
+                        ) : (
+                            <div className='google-login-button'>
+                                <GoogleLogin
+                                    clientId={clientId}                                       
+                                    buttonText='Sing in with Google' 
+                                    onSuccess={onSuccess}
+                                    onFailure={onFailure}
+                                    cookiePolicy={'single_host_origin'}
+                                    isSignedIn={true}
+                                />
+                            </div>/*
+                            <GoogleLogin
+                                clientId={clientId}
+                                className='email-container'   
+                                buttonText='Sing in with Google' 
+                                onSuccess={onSuccess}
+                                onFailure={onFailure}
+                                cookiePolicy={'single_host_origin'}
+                                isSignedIn={true}
+                            />*/
+                        )}
+                        
+                    </div>
+                    
+                </div>
+            </div>
+            
+            
+        </div>
+    );
+}
+
+export default Login
+// npm i react-google-login gapi-script --force
