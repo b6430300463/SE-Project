b1bf14a8c6f541c9cc4ce8a7b73800956f025543
b6430300463 <nanthapong.l@ku.th>
2024-01-29 20:55:35 +0700

    Latest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Import from './Screen/Import';
+import Login from './Screen/Login';
 
 function App() {
   return (
     <div>
-      <Import/>
+      <Login/>
     </div>
   );
 }
