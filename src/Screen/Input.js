18921bc2992931949285ae86bdde7d4d8a494c8b
b6430300463 <nanthapong.l@ku.th>
2024-02-07 20:26:42 +0700

    Update add button

--- src/Screen/Input.js
+++ src/Screen/Input.js
@@ -3,8 +3,11 @@ import './Style/DrawerStyle.css'
 import { FaRegUserCircle } from "react-icons/fa";
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { RiDeleteBin6Fill } from "react-icons/ri";
 const Input = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [LabInput,setLabInput] = useState([]);
 
     const openNav = () => {
         setIsDrawerOpen(true);
@@ -13,6 +16,27 @@ const Input = () => {
     const closeNav = () => {
         setIsDrawerOpen(false);
     };
+    const addLab = () =>{
+        const add=[...LabInput,[]]
+        setLabInput(add)
+    }
+    const submitAlert = () => {
+        Swal.fire({
+            title:"Added successfully!!",
+            icon:"success",
+            confirmButtonText:'Okay'
+        })
+    }
+    const HandleDelete = (index) => {
+        const deleteIndex = [...LabInput]
+        deleteIndex.splice(index,1)
+        setLabInput(deleteIndex)
+    }
+    const HandleInput = (onChangeValue,index) => {
+        const inputData = [...LabInput]
+        inputData[index] = onChangeValue.target.value;
+        setLabInput(inputData)
+    }
     return(
         <div className='input-container'>                         
             <div className='header-bar'>
@@ -32,7 +56,7 @@ const Input = () => {
             </div>
             {/* โค้ดheaderbar */}
 
-            <div className="function-box">
+            <div className="function-box scrollview">
                 <br/>
                 <label><strong>อาจารย์กรอกคำร้องขอเปิดรายวิชา</strong></label>
                 <br/><br/>
@@ -81,19 +105,18 @@ const Input = () => {
                         <option>None</option>
                     </select>
                 </div>
-                <br/><br/>
-
+                <br/><hr style={{background:'#344e41',height:'5px'}}/>
                 <div className='lab'>
                     <label className='checkbox-container2'>ปฏิบัติ
-                        <input type='checkbox'/>
+                        <input type='checkbox' />
                         <span className='checkbox-checkmark'></span>
                     </label>
                     <label className='radiobox-container'>บังคับ
-                        <input type='radio' name="radio2"/>
+                        <input type='radio' name="radio2" />
                         <span className='radiobox-checkmark'></span>
                     </label>
                     <label className='radiobox-container'>เสรี
-                        <input type='radio' name="radio2"/>
+                        <input type='radio' name="radio2" />
                         <span className='radiobox-checkmark'></span>
                     </label>
                     <select name='yr1' id='select1'>
@@ -111,6 +134,7 @@ const Input = () => {
                         <option value='sec3'>3</option>
                     </select>
                     <label for='yr1' id='year-label'>เซคที่</label>
+                    
                 </div>
                 <div className='subject-code'>
                     <label className='lec-code2'><strong>รหัสวิชา</strong></label>
@@ -121,51 +145,116 @@ const Input = () => {
                     <label className='time2'><strong>เวลา</strong></label>
                     <label className='room2'><strong>ห้อง</strong></label>
                     <label className='rq2'><strong>Teacher request</strong></label>
-                   
-                    {/* <label for='lec-code'><strong>รหัสวิชา</strong></label>
-                    <label className='input-codename'><strong>ชื่อวิชา</strong></label><br/>
-                    <label className='input-codeyear'><strong>รหัสสาขา</strong></label><br/>
-                    <select name='lec-code' id='select-code'>
+                </div>
+                <div className='subject-layer'>
+                    <select name='lec-code2' className='select-box'>
                         <option value='none'>None</option>
                         <option value='math I'>math I </option>
                         <option value='math II'>math II </option>
                         <option value='math III'>math III </option>
-                    </select> 
-                    
-                    <input type= "text" id= "lec-name" name="lec-name"></input>
-
-                    <input type= "text" id= "code-year" name="code-year"></input> */}
-
-                </div>
-                <div className='subject-layer'>
-                    <select name='lec-code2' id='select-code'>
-                            <option value='none'>None</option>
-                            <option value='math I'>math I </option>
-                            <option value='math II'>math II </option>
-                            <option value='math III'>math III </option>
-                    </select> 
-                    <input type= "text" id= "lec-name2" name="lec-name"></input>
-                    <input type= "text" id= "year-code2" name="year-code2"></input>
-                    <input type= "text" id= "people2" name="people2"></input>
-                    <input type= "text" id= "day2" name="day2"></input>
+                    </select>
+                    <input type="text" id="lec-name2" name="lec-name"></input>
+                    <input type="text" id="year-code2" name="year-code2"></input>
+                    <input type="text" id="people2" name="people2"></input>
+                    <input type="text" id="day2" name="day2"></input>
                     <select name='time2-1' id='time2-1'>
-                            <option value='None'>None</option>
-                            <option value='08.00'>08.00 </option>
-                            <option value='08.30'>08.30 </option>
-                            <option value='09.00'>09.00 </option>
-                    </select> 
+                        <option value='None'>None</option>
+                        <option value='08.00'>08.00 </option>
+                        <option value='08.30'>08.30 </option>
+                        <option value='09.00'>09.00 </option>
+                    </select>
                     <label className='ke2'>  - </label>
                     <select name='time2-2' id='time2-2'>
-                            <option value='none'>None</option>
-                            <option value='08.00'>08.00 </option>
-                            <option value='08.30'>08.30 </option>
-                            <option value='09.00'>09.00 </option>
-                    </select> 
-                    <input type= "text" id= "room2" name="room2"></input>
-                    <input type= "text" id= "rq2" name="rq2"></input>
+                        <option value='none'>None</option>
+                        <option value='08.00'>08.00 </option>
+                        <option value='08.30'>08.30 </option>
+                        <option value='09.00'>09.00 </option>
+                    </select>
+                    <input type="text" id="room2" name="room2"></input>
+                    <input type="text" id="rq2" name="rq2"></input>
                 </div>
+                <br/><hr/>
+                {LabInput.map((data, index) => {
+                    return(
+                        <>
+                            <div key={index} className='lab'>
+                                <label className='checkbox-container2'>ปฏิบัติ
+                                    <input type='checkbox' onChange={e => HandleInput(e,index)} />
+                                    <span className='checkbox-checkmark'></span>
+                                </label>
+                                <label className='radiobox-container'>บังคับ
+                                    <input type='radio' name={"radio"[index]} onChange={e => HandleInput(e,index)}/>
+                                    <span className='radiobox-checkmark'></span>
+                                </label>
+                                <label className='radiobox-container'>เสรี
+                                    <input type='radio' name={"radio"[index]} onChange={e => HandleInput(e,index)}/>
+                                    <span className='radiobox-checkmark'></span>
+                                </label>
+                                <select name='yr1' id='select1' onChange={e => HandleInput(e,index)}>
+                                    <option value='none'>None</option>
+                                    <option value='year1'>T12(1)</option>
+                                    <option value='year2'>T12(2)</option>
+                                    <option value='year3'>T12(3)</option>
+                                    <option value='year4'>T12(4)</option>
+                                </select>
+                                <label for='yr1' id='year-label'>ชั้นปี</label>
+                                <select name='sec' id='sec-select' onChange={e => HandleInput(e,index)}>
+                                    <option value='none'>None</option>
+                                    <option value='sec1'>1</option>
+                                    <option value='sec2'>2</option>
+                                    <option value='sec3'>3</option>
+                                </select>
+                                <label for='yr1' id='year-label'>เซคที่</label>
+                                <button id='bin' onClick={() => HandleDelete(index)}><RiDeleteBin6Fill size={20}/></button>
+                            </div>
+                            <div className='subject-code'>
+                                <label className='lec-code2'><strong>รหัสวิชา</strong></label>
+                                <label className='lec-name2'><strong>ชื่อวิชา</strong></label>
+                                <label className='year-code2'><strong>รหัสสาขา</strong></label>
+                                <label className='people2'><strong>จำนวน</strong></label>
+                                <label className='day2'><strong>วัน</strong></label>
+                                <label className='time2'><strong>เวลา</strong></label>
+                                <label className='room2'><strong>ห้อง</strong></label>
+                                <label className='rq2'><strong>Teacher request</strong></label>
+                            </div>
+                            <div className='subject-layer'>
+                                <select name='lec-code2' className='select-box'>
+                                    <option value='none'>None</option>
+                                    <option value='math I'>math I </option>
+                                    <option value='math II'>math II </option>
+                                    <option value='math III'>math III </option>
+                                </select>
+                                <input type="text" id="lec-name2" name="lec-name"></input>
+                                <input type="text" id="year-code2" name="year-code2"></input>
+                                <input type="text" id="people2" name="people2"></input>
+                                <input type="text" id="day2" name="day2"></input>
+                                <select name='time2-1' id='time2-1'>
+                                    <option value='None'>None</option>
+                                    <option value='08.00'>08.00 </option>
+                                    <option value='08.30'>08.30 </option>
+                                    <option value='09.00'>09.00 </option>
+                                </select>
+                                <label className='ke2'>  - </label>
+                                <select name='time2-2' id='time2-2'>
+                                    <option value='none'>None</option>
+                                    <option value='08.00'>08.00 </option>
+                                    <option value='08.30'>08.30 </option>
+                                    <option value='09.00'>09.00 </option>
+                                </select>
+                                <input type="text" id="room2" name="room2"></input>
+                                <input type="text" id="rq2" name="rq2"></input>
+                            </div>
+                            <br/><hr/>
+                        </>
+                    );
+                })}
+                <div className='add-lab'>
+                    <button type='button' onClick={() => addLab()}>+</button>
+                </div>
+            </div>
 
-                
+            <div className='sub-box'>
+                <button type='submit' className='submit-btn' id='submit-input' onClick={submitAlert}><strong>Submit</strong></button>
             </div>
         </div>
     );
