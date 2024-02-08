236ec504b3576bc27394eb8eb588a295167ffef5
NpatPanya <Npatonpym@gmail.com>
2024-02-08 22:00:07 +0700

    Import show data เสร็จแต่ว่ายังไม่ได้จัดหน้าให้สวย กับ save to database

--- src/Screen/Import.js
+++ src/Screen/Import.js
@@ -8,20 +8,21 @@ import { FaRegUserCircle } from "react-icons/fa";
 import Swal from 'sweetalert2';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import * as XLSX from 'xlsx';
+import {read, utils, writeFile } from "xlsx"
+
 
 // npm  i sweetalert2 react-icon ด้วย
 const Import = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const [typeError,setTypeError] = useState(null);
-    const [excelData, setExcelData] = useState(null);
-    const [excelFile,setExcelFile] = useState(null);
+    const [users, setUsers] = useState([]);
+    
     const submitAlert = () => {
         Swal.fire({
             title:"Added successfully!!",
             icon:"success",
             confirmButtonText:'Okay'
         })
+        console.log("checkError",users)
     }
     const openNav = () => {
         setIsDrawerOpen(true);
@@ -30,35 +31,48 @@ const Import = () => {
     const closeNav = () => {
         setIsDrawerOpen(false);
     };
-    const handleFile = (e) => {
-        let fileTypes = ['application/vnd.ms-excel','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','text/csv']
-        let selectedFile = e.target.files[0];
-        if(selectedFile){
-            if(selectedFile&&fileTypes.includes(selectedFile.type)){
-                setTypeError(null);
-                let reader = new FileReader();
-                reader.readAsArrayBuffer(selectedFile)
-                reader.onload = (e) => {
-                    setExcelFile(e.target.result);
+    const handleImport = $event => {
+        const files =$event.target.files;
+        if (files.length){
+            const file =files[0];
+            const reader = new FileReader();
+            reader.onload = event => {
+                const wb = read(event.target.result);
+                const sheets = wb.SheetNames;
+                
+                if(sheets.length){
+                    const rows = utils.sheet_to_json(wb.Sheets[sheets[0]]);
+                    setUsers(rows);
+
                 }
-            }else{
-                setTypeError('Please select only excel file types')
-                setExcelFile(null);
-            }
-        }else{
-            console.log('Please select your file')
+            };
+            
+            reader.readAsArrayBuffer(file);
+            
+
         }
-    }
-    const handleFileSubmit = (e) => {
-        e.preventDefault();
-        if(excelFile !== null){
-            const workbook = XLSX.read(excelFile,{type:'buffer'});
-            const worksheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[worksheetName];
-            const data = XLSX.utils.sheet_to_json(worksheet);
-            setExcelData(data.slice(0,10));
-        }
-    }
+    };
+
+    // const handleExport = () => {
+    //     const headings = [["subject id","year","subject","credit"]];
+    //     const wb = utils.book_new();
+    //     const wb = utils.json_to_sheet([]);
+    //     utils.sheet_add_aoa(we,headings);
+    //     utils.sheet_add_json(ws,users,{origin:"A2",skipHeader: true});
+    //     utils.book_append_sheet(wb,ws,"Report");
+    //     writeFile(wb,"Report.xlsx");
+    // };
+
+    // const handleFileSubmit = (e) => {
+    //     e.preventDefault();
+    //     if(excelFile !== null){
+    //         const workbook = XLSX.read(excelFile,{type:'buffer'});
+    //         const worksheetName = workbook.SheetNames[0];
+    //         const worksheet = workbook.Sheets[worksheetName];
+    //         const data = XLSX.utils.sheet_to_json(worksheet);
+    //         setExcelData(data.slice(0,10));
+    //     }
+    // }
     return(
         <div className="import-container">
             <div className="header-bar">
@@ -78,41 +92,101 @@ const Import = () => {
             </div>
 
             {/* เริ่มตั้งแต่ตรงนี้ */}
-            <form onSubmit={handleFileSubmit}>
-                <div className='input' >
+            <form onSubmit={handleImport}>
+                <div className='input row mb-2 mt-5' >
                     <FaSearch id='search-icon'/>
                     <input className="search-box" placeholder="Search..." ></input>
-                    <input type="file" className='file-input' onClick={handleFile}/>
-                    <button type='submit' id='import-icon'><CiImport  size={30}/></button>
+                    {/* <button type='submit' id='import-icon'><CiImport  size={30}/></button> */}
                     <button id='bin-icon'><RiDeleteBin6Fill size={30}/></button>
+                
+                    <div className ="col-sm-12">
+                        <div className ="row">
+                            <div className = "col-md-6">
+                                <div className = "input-group">
+                                    <div className = "custom-file">
+                                        <input
+                                            type = "file"
+                                            name = "file"
+                                            className = "custom-file-input"
+                                            id = "inputGroupFile"
+                                            require
+                                            onChange ={handleImport}
+                                            accept = ".csv,application/vnd.openxmlformats-officedocument.spreadsheet"
+                                            />
+                                            <label className = "custom-file-label" htmlFor = "inputGroupFile">
+                                                Choose file
+                                            </label>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* <div className ="col - md -6">
+                                <button onClick = {handleExport}
+                                className = "btn btn -secondary float-right"
+                                >
+                                    Export <i className = "fa fa-download"></i>
+                                </button>
+
+                            </div> */}
+                        </div>
+                    </div>
                 </div>
             </form>
-            {typeError&&(
-                <div role='alert'>{typeError}</div>
-            )}
-            <div className='scrollv'>
-                {excelData?(
-                    <div id="subject">
-                        <thead>
-                            <tr>
-                                {Object.keys(excelData[0]).map((key) => (
-                                    <th key={key}>{key}</th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {excelData.map((individualExcelData,index) => (
-                                <tr key={index}>
-                                    {Object.keys(individualExcelData).map((key) => (
-                                        <td key={key}>{individualExcelData}</td>
-                                    ))}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </div>
-                ):(
-                    <div id="subject">No File is uploaded yet!</div>
-                )}
+
+                        <div className = "scrollv">
+                            <div className = "row">
+                                <div className = "col-md-12">
+                                    <table className ="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope = "col">order</th>
+                                                <th scope = "col">subjectID</th>
+                                                <th scope = "col">year</th>
+                                                <th scope = "col">subject</th>
+                                                <th scope = "col">credit</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {users.length ? (
+                                                 users.map((user,index) => (
+                                                    <tr key ={index}>
+                                                        {/* <th scope = "row">{index}</th> */}
+                                                        <td>{user.order}</td>
+                                                        <td>{user.subjectid}</td>
+                                                        <td>{user.year}</td>
+                                                        <td>{user.subject}</td>
+                                                        <td>{user.credit}</td>
+
+                                                    </tr>
+                                                ))
+                                        
+                                            ):(
+                                                <tr>
+                                                    <td colSpan="6" className = "text-center">
+                                                        No Users Found.
+                                                    </td>
+                                                </tr>
+                                            )}
+                                        </tbody>
+                                               
+                                    </table>
+                        
+                                </div>
+                            </div>
+
+                        </div>
+                        {/* <div className = "col-md-6">
+                            <button
+                            onCLick = {handleExport}
+                            className = "btn btn-secondary float-right"
+                            >
+                                Export<i className = "fa fa-download"></i>
+                            </button>
+                        </div> */}
+                    
+            
+
+
+            
             {/* {
                 DataDB.map( database => {
                     return(
@@ -132,7 +206,7 @@ const Import = () => {
             } */}
             {/* ถึงตรงนี้ */}
 
-            </div>
+            
             <div className='submit'>
                 <button type='submit' className="submit-btn" onClick={submitAlert}><strong>Submit</strong></button>
             </div>
